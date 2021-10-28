193659f848b1b69468f1cd371954294afe779c52
kathmandu777 <mahhakatomanato@gmail.com>
2021-10-28 15:49:07 +0900

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,17 @@ wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-com
 chmod a+x ~/.git-completion.bash
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 chmod a+x ~/.git-prompt.sh
+
+echo '
+######### my-settings ########
+# Settings that apply only to this environment that should be applied before loading my-settings.
+source ~/.git-completion.bash # Need. The path should be changed depending on the environment.
+source ~/.git-prompt.sh # Need. The path should be changed depending on the environment.
+
+# Apply my-settings. my_settings_path should be changed to suit your environment
+my_settings_path="$HOME/my-settings" # change
+source "${my_settings_path}/base"
+
+# Settings that apply only to this environment that should be applied after loading my-settings.
+cd ~
+' >> ~/.bashrc
